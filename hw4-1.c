e4aada319460e4254e95ce656cbc2cb1adc7d7a6
--unset <--unset>
2023-10-29 23:37:51 +0800

    update hw4-2, fix hw4-1 logic

--- hw4-1.c
+++ hw4-1.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <assert.h>
+#define ARRAYSIZE 50
 
 int isPrime(int);
 
 int main() {
   int input;
   scanf("%d", &input);
-  assert(input >= 1 && input <= 50);
+  assert(input >= 1 && input <= ARRAYSIZE);
+  int arr[ARRAYSIZE];
   
   for (int i = 0; i < input; i++) {
     int in;
     scanf("%d", &in);
-    if (isPrime(in)) {
+    arr[i] = in;
+  }
+
+  for (int i = 0; i < input; i++) {
+    if (isPrime(arr[i])) {
       printf("YES");
     } else {
       printf("NO");
